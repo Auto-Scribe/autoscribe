b484695796e7ec7d8af9331b20157572520041b9
Gabe C <113395303+GabeVC@users.noreply.github.com>
2025-12-12 01:21:34 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,52 @@
+from .data_structures import Note, Chord, PianoRoll, MusicalSegment, NoteType
+
+from .midi_parser import MidiParser, MidiParserError, load_midi
+
+from .rhythm_quantizer import RhythmQuantizer, QuantizationConfig, quantize_piano_roll
+
+from .chord_detector import (
+    ChordDetector,
+    ChordDetectionConfig,
+    detect_chords,
+    get_chord_name,
+)
+
+from .voice_separator import VoiceSeparator, VoiceSeparationConfig, separate_voices
+
+from .hand_assigner import HandAssigner, HandAssignmentConfig, Hand, assign_hands
+
+from .musicxml_exporter import MusicXMLExporter, export_to_musicxml
+
+__all__ = [
+    # Data structures
+    "Note",
+    "Chord",
+    "PianoRoll",
+    "MusicalSegment",
+    "NoteType",
+    # Parser
+    "MidiParser",
+    "MidiParserError",
+    "load_midi",
+    # Quantizer
+    "RhythmQuantizer",
+    "QuantizationConfig",
+    "quantize_piano_roll",
+    # Chord Detector
+    "ChordDetector",
+    "ChordDetectionConfig",
+    "detect_chords",
+    "get_chord_name",
+    # Voice Separator
+    "VoiceSeparator",
+    "VoiceSeparationConfig",
+    "separate_voices",
+    # Hand Assigner
+    "HandAssigner",
+    "HandAssignmentConfig",
+    "Hand",
+    "assign_hands",
+    # MusicXML Exporter
+    "MusicXMLExporter",
+    "export_to_musicxml",
+]
